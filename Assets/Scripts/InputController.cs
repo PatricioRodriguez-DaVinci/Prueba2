aa2a30381be310943d8deea107b9ab92e2d05f05
Patricio Rodríguez - DaVinci <patricio.rodriguez@davinci.edu.ar>
2023-05-11 20:31:13 -0300

    ShootController
    Puede disparar haciendo click.
    Recarga con 'R'.
    Tiene conteo de munición y cadencia de disparo.
    Tiene tiempo de espera después de la recarga.
    Solo puede recargar si tiene poca munición.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -7,12 +7,14 @@ public class InputController : MonoBehaviour
     [Header("---------- Movement Imputs ----------\n")]
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode runKey = KeyCode.LeftShift;
+    public KeyCode reloadKey = KeyCode.R;
 
     public float xAxis, zAxis;
 
     public bool isJumping = false;
     public bool isRunning = false;
     public bool isShooting = false;
+    public bool isReloading = false;
     void Update()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
@@ -20,7 +22,7 @@ public class InputController : MonoBehaviour
 
         if (Input.GetKeyDown(jumpKey)) isJumping = true;
         if (Input.GetKey(runKey)) isRunning = true; else isRunning = false;
-
         if (Input.GetMouseButton(0)) isShooting = true; else isShooting = false;
+        if (Input.GetKeyDown(reloadKey)) isReloading = true;
     }
 }
--- Assets/Scripts/ShootController.cs
+++ Assets/Scripts/ShootController.cs
@@ -7,6 +7,14 @@ public class ShootController : MonoBehaviour
 {
     private InputController _inputController;
 
+    public GameObject bulletPrefab;
+    public Transform spawnPoint;
+    [SerializeField] private float _spawnRate = 0.1f;
+    [SerializeField] private float _reloadTime = 2f;
+    private float _timeSinceLastSpawn = 0f;
+    [SerializeField] private int _ammoCount = 10;
+    private int _minAmmo = 3;
+
     private void Start()
     {
         _inputController = GetComponent<InputController>();
@@ -14,9 +22,21 @@ public class ShootController : MonoBehaviour
 
     void LateUpdate()
     {
-        if(_inputController.isShooting)
+        _timeSinceLastSpawn += Time.deltaTime;
+
+        if (_inputController.isShooting && _timeSinceLastSpawn >= _spawnRate && _ammoCount > 0)
         {
             Debug.Log("Disparar");
+            Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+            _timeSinceLastSpawn = 0f;
+            _ammoCount--;
+        }
+
+        if (_inputController.isReloading && _ammoCount <= _minAmmo)
+        {
+            _timeSinceLastSpawn = _reloadTime * -1f;
+            _ammoCount = 10;
+            _inputController.isReloading = false;
         }
     }
 }
