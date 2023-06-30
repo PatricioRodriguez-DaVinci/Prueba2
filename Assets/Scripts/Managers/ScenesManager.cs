45e0ce7d02b25d3bd10cf454938e555053407a76
Patricio Rodríguez - DaVinci <patricio.rodriguez@davinci.edu.ar>
2023-06-30 17:23:21 -0300

    Habemus menú
    Diseño gráfico mi pasión

---
+++ Assets/Scripts/Managers/ScenesManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public bool       isLevelCompleted;
+
+    void Start()
+    {
+        isLevelCompleted = false;
+    }
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.P)) || isLevelCompleted)
+        {
+            YouWin();
+        }
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+        Debug.Log("Closed");
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+        Debug.Log("Game Over");
+    }
+
+    void YouWin()
+    {
+        SceneManager.LoadScene("YouWin");
+        Debug.Log("You Win");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+        Debug.Log("Level 1");
+    }
+}
